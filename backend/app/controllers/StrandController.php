1b90575c125ac26c556befc2cca9719a1d26806f
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2026-06-26 17:59:58 +0800

    Add model imports to StrandController

--- backend/app/controllers/StrandController.php
+++ backend/app/controllers/StrandController.php
@@ -5,6 +5,9 @@ require_once __DIR__ . '/../../config/Database.php';
 header('Content-Type: application/json');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../models/AuditLogModel.php';
+require_once __DIR__ . '/../models/ScholarshipCriteriaModel.php';
+require_once __DIR__ . '/../models/StaffAccountModel.php';
 
 use App\Constants\Action;
 use App\Models\AuditLogModel;
