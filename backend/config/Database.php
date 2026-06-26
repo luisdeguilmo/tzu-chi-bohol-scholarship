45acd4096844cf6e1e3baca30404fe7508c51dd8
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2026-06-26 16:56:25 +0800

    Add port and CA certificate to Database connection

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -7,17 +7,21 @@ use PDO;
 class Database
 {
     private $host;
+    private $port;
     private $db_name;
     private $username;
     private $password;
+    private $ca_cert;
     private $conn;
 
     public function __construct()
     {
         $this->host = getenv('DB_HOST');
+        $this->port = getenv('DB_PORT');
         $this->db_name = getenv('DB_NAME');
         $this->username = getenv('DB_USERNAME');
         $this->password = getenv('DB_PASSWORD');
+        $this->ca_cert = getenv('DB_CA_CERT');
     }
 
     public function getConnection()
@@ -25,13 +29,21 @@ class Database
         $this->conn = null;
 
         try {
+
             $this->conn = new PDO(
-                "mysql:host={$this->host};dbname={$this->db_name}",
+                "mysql:host={$this->host};port={$this->port};dbname={$this->db_name};sslmode=verify-ca",
                 $this->username,
                 $this->password,
+                [
+                    PDO::MYSQL_ATTR_SSL_CA => $this->ca_cert,
+                ]
             );
 
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
         } catch (\PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
         }
