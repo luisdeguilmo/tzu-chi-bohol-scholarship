f91c33fa767835a3eb1a6062c6b88988f568a041
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2026-06-27 08:55:58 +0800

    Fix case sensitivity in service file inclusion

--- backend/app/controllers/CoeAndGradesController.php
+++ backend/app/controllers/CoeAndGradesController.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../config/Database.php';
-require_once __DIR__ . '/../services/CoeAndGradesService.php';
+require_once __DIR__ . '/../Services/CoeAndGradesService.php';
 require_once __DIR__ . '/../Models/AuditLogModel.php';
 
 use App\Constants\Action;
