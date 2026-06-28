868c17597198c278fc1f8c3d8bf46a237356d76f
Luis Deguilmo <deguilmoluis02@gmail.com>
2026-06-29 01:29:48 +0800

    fix YearLevelController.php

--- backend/app/Controllers/YearLevelController.php
+++ backend/app/Controllers/YearLevelController.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../../config/Database.php';
 header('Content-Type: application/json');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../Middleware/Auth.php';
 
 use App\Models\ScholarModel;
 use Config\Database;
