8a2f47e4ef75891dd2b82947279379a3fd0b8b5e
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2026-06-28 22:41:28 +0800

    Refactor Database class to use environment variables

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace Config;
 
 use PDO;
@@ -6,35 +7,50 @@ use PDO;
 class Database
 {
     private $host;
+    private $port;
     private $db_name;
     private $username;
     private $password;
+    private $ca_cert;
     private $conn;
 
     public function __construct()
     {
-        $this->host = 'localhost';
-        $this->db_name = 'tzu_chi_scholarship';
-        $this->username = 'root';
-        $this->password = '';
+        $this->host = getenv('DB_HOST');
+        $this->port = getenv('DB_PORT');
+        $this->db_name = getenv('DB_NAME');
+        $this->username = getenv('DB_USERNAME');
+        $this->password = getenv('DB_PASSWORD');
+        $this->ca_cert = getenv('DB_CA_CERT');
     }
 
     public function getConnection()
-    {
-        $this->conn = null;
+{
+    $this->conn = null;
 
-        try {
-            $this->conn = new PDO(
-                "mysql:host={$this->host};dbname={$this->db_name}",
-                $this->username,
-                $this->password,
-            );
+    try {
 
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (\PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        if (!empty($this->ca_cert)) {
+            $options[PDO::MYSQL_ATTR_SSL_CA] = $this->ca_cert;
         }
 
-        return $this->conn;
+        $this->conn = new PDO(
+            "mysql:host={$this->host};port={$this->port};dbname={$this->db_name}",
+            $this->username,
+            $this->password,
+            $options
+        );
+
+    } catch (\PDOException $e) {
+        error_log($e->getMessage());
+        return null;
     }
+
+    return $this->conn;
+}
 }
