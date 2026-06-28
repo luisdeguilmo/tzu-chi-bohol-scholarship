328af50abad8d9c30265c81a3220d2a633a143a2
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2026-06-28 23:50:46 +0800

    Add Action constants import to CoeAndGradesController

--- backend/app/controllers/CoeAndGradesController.php
+++ backend/app/controllers/CoeAndGradesController.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../config/Database.php';
 require_once __DIR__ . '/../Services/CoeAndGradesService.php';
 require_once __DIR__ . '/../Models/AuditLogModel.php';
+require_once __DIR__ . '/../constants/Action.php';
 
 use App\Constants\Action;
 use App\Models\AuditLogModel;
