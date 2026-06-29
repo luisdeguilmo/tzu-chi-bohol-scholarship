636af93ccc137965eebd672afd5294f4b14e4855
Luis Deguilmo <142017049+luisdeguilmo@users.noreply.github.com>
2026-06-30 00:29:54 +0800

    Fix case sensitivity in YearController file inclusion

--- backend/app/api/years.php
+++ backend/app/api/years.php
@@ -4,6 +4,6 @@ $allowedMethods = ['GET'];
 $allowedHeaders = ['Content-Type', 'Authorization', 'X-Requested-With'];
 
 require_once __DIR__ . '/../../config/bootstrap.php';
-require_once __DIR__ . '/../controllers/YearController.php';
+require_once __DIR__ . '/../Controllers/YearController.php';
 
 ?>
